7797c8c2e8c43ab3c899b187bfc2ad497b5be0a6
“Kes <kesia.development@gmail.com>
2021-02-16 19:44:57 +0100

    Add bootstrap container to search form div

--- extension.js
+++ extension.js
@@ -20,20 +20,27 @@ function getSearchBar(){
 			<title>Pictury</title>
 		</head>
 		<body>
-			<!-- Instructions : -->
-			<div class="instructions">
-				<h2>How to Use Pictory:</h2>
-				<p>1. Use the search box below to find your images.</p>
-				<p>2. Type in keywords and hit enter.</p>
-				<p>3. From the search results, select an image with your mouse.</p>
-				<p>4. Double click an image to download it to your workspace.</p>
+			<div class="container-fluid">
+				<!-- Instructions : -->
+				<div class="instructions">
+					<h2>How to Use Pictory:</h2>
+					<p>1. Use the search box below to find your images.</p>
+					<p>2. Type in keywords and hit enter.</p>
+					<p>3. From the search results, select an image with your mouse.</p>
+					<p>4. Double click an image to download it to your workspace.</p>
+				</div>
+
+				<!-- Search Input : -->
+				<div class="searchbox">
+					<h2>Search Here:</h2>
+					<form id="myForm" autocomplete="off">
+						<div class ="form-group">
+							<input type="text" class ="form-control" id="search" placeholder="Search image" required>
+						</div>
+					</form>
+				</div>
 			</div>
-			<!-- Search Input : -->
-			<h2>Search Here:</h2>
-			<form id="myForm" autocomplete="off">
-				<div class ="form-group">
-					<input type="text" class ="form-control" id="search" placeholder="Search image">
-		
+
 			<!-- Bootstrap Js, jQuery and Popper.js : -->		
 			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
