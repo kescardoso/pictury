884740f09aa4762f1ff2ef5c6c6ddbfc0507a62d
“Kes <kesia.development@gmail.com>
2021-02-18 22:04:23 +0100

    Started dynamic search form installation

--- extension.js
+++ extension.js
@@ -99,21 +99,40 @@ function getSearchBar(){
 
 			<div class="container pt-2 pb-2">
 				<!-- Search Input : -->
-				<div class="searchbox">
+				
 				<h6 class="text-uppercase">Search Here:</h6>
-					<form id="myForm" autocomplete="off">
+					<form id="pictory-form" autocomplete="off">
 						<div class ="form-group">
-							<input type="text" class ="form-control" id="search" placeholder="Search image" required>
+							<input type="text" 
+							class ="form-control" 
+							id="search" 
+							placeholder="Search image" required>
 						</div>
 					</form>
-				</div>
+				
 			</div>
 
 			<!-- Bootstrap Js, jQuery and Popper.js : -->		
 			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>			
-		`
+		
+			<--! Search Box jQuery Script : -->
+			<script>
+				$("#pictury-form").submit(function(event){
+					event.preventDefault()
+
+					var search = $("#search").val()
+
+					var url = ""
+
+					$.ajax({
+						method: 'GET',
+						url:''
+					})
+				})
+			</script>
+		</body>`
 	return html;
 }
 
